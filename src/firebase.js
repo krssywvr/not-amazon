69472cea2b9267350d5941e605e698d493751aeb
Kristine Erickson <ericksonk09@gmail.com>
2021-06-05 15:34:17 -0400

    Create Header nav component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB7MIIL9Fjuf02uaZcayaXhUHP6ya5-5DQ",
+    authDomain: "definitely-not-87494.firebaseapp.com",
+    projectId: "definitely-not-87494",
+    storageBucket: "definitely-not-87494.appspot.com",
+    messagingSenderId: "46260533933",
+    appId: "1:46260533933:web:a3958abe5f920c56d6e424",
+    measurementId: "G-KK8J7Y13L0"
+};
