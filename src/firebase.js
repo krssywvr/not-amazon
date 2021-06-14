297232753d165e0fd67898aed5a629d532b23b14
Kristine Weaver <krssywvr@gmail.com>
2021-06-14 16:02:20 -0400

    create login page, set up user authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyB7MIIL9Fjuf02uaZcayaXhUHP6ya5-5DQ",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:46260533933:web:a3958abe5f920c56d6e424",
     measurementId: "G-KK8J7Y13L0"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
